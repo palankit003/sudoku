f204e9b303f2c6561935306935f61ff07c0c42ce
palankit003 <palankit003.ap@gmail.com>
2023-07-27 12:57:56 +0530

    add: About Page

--- landing-page/src/pages/About/About.tsx
+++ landing-page/src/pages/About/About.tsx
@@ -1,11 +1,11 @@
 import Character from "../Home/Character/Character";
 import Alex from "../../assets/lead guitarist.png";
-import Lisa from "../../assets/Lisa.png";
-import Max from "../../assets/Bass Guitarist.png";
-import Sarah from "../../assets/Sarah-Keyboardist.jpg";
-import Jake from "../../assets/drummer.jpg";
-import Michelle from "../../assets/lead vocalist.jpg";
-import Evan from "../../assets/multi-instrumentalist 2.jpg";
+// import Lisa from "../../assets/Lisa.png";
+// import Max from "../../assets/Bass Guitarist.png";
+// import Sarah from "../../assets/Sarah-Keyboardist.jpg";
+// import Jake from "../../assets/drummer.jpg";
+// import Michelle from "../../assets/lead vocalist.jpg";
+// import Evan from "../../assets/multi-instrumentalist 2.jpg";
 const About = () => {
   return (
     <>
